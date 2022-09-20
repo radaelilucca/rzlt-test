1566dbad38743a70c3e66e50969af7f0b5988c7e
Lucca Radaeli <radaelidev@gmail.com>
2022-09-20 13:42:43 -0300

    feat(components/LogoHeading): add mobile responsiveness

--- src/components/LogoHeading/styles.ts
+++ src/components/LogoHeading/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { breakpoints } from '~/styles';
+
 export const Container = styled.div`
   display: flex;
   align-items: center;
@@ -9,5 +11,18 @@ export const Container = styled.div`
   h1 {
     font-size: 4rem;
     font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+  }
+
+  @media (max-width: ${breakpoints.mobile}px) {
+    gap: 1.5rem;
+
+    h1 {
+      font-size: 2.5rem;
+    }
+
+    svg {
+      max-width: 3.5rem;
+      max-height: 3.5rem;
+    }
   }
 `;
