1d9e419a521d89e538ce9d682cb78cf3e7666b45
Lucca Radaeli <radaelidev@gmail.com>
2022-09-20 21:07:30 -0300

    chore(assets/icons): add some icons

--- src/assets/svg/icons/index.ts
+++ src/assets/svg/icons/index.ts
@@ -1,4 +1,19 @@
+import { ReactComponent as ArrowBackIcon } from './arrow-back.svg';
+import { ReactComponent as IssuesIcon } from './disc.svg';
+import { ReactComponent as ForksIcon } from './git-fork.svg';
 import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as GoBackIcon } from './navigate-back.svg';
+import { ReactComponent as GoForwardIcon } from './navigate-forward.svg';
 import { ReactComponent as LoadingIcon } from './tail-spin.svg';
+import { ReactComponent as WatchersIcon } from './watchers.svg';
 
-export { Logo, LoadingIcon };
+export {
+  Logo,
+  LoadingIcon,
+  ArrowBackIcon,
+  IssuesIcon,
+  ForksIcon,
+  GoBackIcon,
+  GoForwardIcon,
+  WatchersIcon,
+};
