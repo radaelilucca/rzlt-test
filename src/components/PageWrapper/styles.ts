eefd1569aed2befa2de10a22336bafb47f039f0c
Lucca Radaeli <radaelidev@gmail.com>
2022-09-21 00:10:48 -0300

    fix(components/PageWrapper): add some horizontal padding on smaller views

--- src/components/PageWrapper/styles.ts
+++ src/components/PageWrapper/styles.ts
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
+import { breakpoints } from '~/styles';
+
 export const Container = styled.main`
   height: 100%;
 
   width: 100%;
-  max-width: 1440px;
+  max-width: ${breakpoints.desktop}px;
+
+  @media (max-width: ${breakpoints.desktop}px) {
+    padding: 0 2rem;
+  }
 `;
