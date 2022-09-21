7e39f5e9538b22a3a77fc77e48c1628920178236
Lucca Radaeli <radaelidev@gmail.com>
2022-09-21 00:07:36 -0300

    chore(assets/svg): add icons, add sad emoji and rename arrow

--- src/assets/svg/icons/index.ts
+++ src/assets/svg/icons/index.ts
@@ -1,19 +1,21 @@
-import { ReactComponent as ArrowBackIcon } from './arrow-back.svg';
+import { ReactComponent as ArrowForwardIcon } from './arrow-forward.svg';
 import { ReactComponent as IssuesIcon } from './disc.svg';
 import { ReactComponent as ForksIcon } from './git-fork.svg';
 import { ReactComponent as Logo } from './logo.svg';
 import { ReactComponent as GoBackIcon } from './navigate-back.svg';
 import { ReactComponent as GoForwardIcon } from './navigate-forward.svg';
+import { ReactComponent as StarIcon } from './star.svg';
 import { ReactComponent as LoadingIcon } from './tail-spin.svg';
 import { ReactComponent as WatchersIcon } from './watchers.svg';
 
 export {
   Logo,
   LoadingIcon,
-  ArrowBackIcon,
+  ArrowForwardIcon,
   IssuesIcon,
   ForksIcon,
   GoBackIcon,
   GoForwardIcon,
   WatchersIcon,
+  StarIcon,
 };
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,1 +1,4 @@
+import { ReactComponent as SadEmoji } from './sad-emoji.svg';
+
+export { SadEmoji };
 export * from './icons';
